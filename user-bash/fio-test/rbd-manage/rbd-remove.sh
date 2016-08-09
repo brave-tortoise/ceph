1bb16db23229e7bcb69e0d536901e1fe7a6d7ba1
brave-tortoise <wugy10@gmail.com>
2016-08-09 16:57:10 +0800

    update bash

---
+++ user-bash/fio-test/rbd-manage/rbd-remove.sh
@@ -0,0 +1,5 @@
+rbd rm test1 -p hdd-pool
+rbd rm test2 -p hdd-pool
+rbd rm test3 -p hdd-pool
+rbd rm test4 -p hdd-pool
+rados -p ssd-pool cache-flush-evict-all
--- user-bash/manage-bash/config/tier-config.sh
+++ user-bash/manage-bash/config/tier-config.sh
@@ -2,7 +2,7 @@
 ceph osd pool set ssd-pool hit_set_type bloom
 ceph osd pool set ssd-pool hit_set_count 32
 ceph osd pool set ssd-pool hit_set_period 1800
-ceph osd pool set ssd-pool min_read_recency_for_promote 4
+ceph osd pool set ssd-pool min_read_recency_for_promote 2
 ceph osd pool set ssd-pool target_max_bytes 100000000000
 ceph osd pool set ssd-pool target_max_objects 100000
 ceph osd pool set ssd-pool cache_target_dirty_ratio 0.4
