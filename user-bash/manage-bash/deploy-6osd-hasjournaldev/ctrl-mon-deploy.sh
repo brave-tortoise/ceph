caf6c89b4fcbb699c74a7ea19b35b8bad1f49cea
brave-tortoise <wugy10@gmail.com>
2016-11-19 20:12:57 +0800

    deploy bash

--- user-bash/manage-bash/deploy-6osd-hasjournaldev/ctrl-mon-deploy.sh
+++
@@ -1,5 +0,0 @@
-for((i=1; i<4; i++))
-do
-	scp mon-deploy.sh node$i:~
-	ssh node$i "./mon-deploy.sh node$i && rm -f mon-deploy.sh"
-done
--- user-bash/manage-bash/deploy-single-osd-hasjournal/ctrl-osd-deploy.sh
+++
@@ -1,5 +0,0 @@
-for((i=1; i<4; i++))
-do
-	scp osd-deploy.sh node$i:~
-	ssh node$i "./osd-deploy.sh && rm -f osd-deploy.sh"
-done
