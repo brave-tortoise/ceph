5fc5beb7935cb3cf463e02bf78bd0ceee142db01
brave-tortoise <wugy10@gmail.com>
2016-11-19 19:49:43 +0800

    deploy bash

--- user-bash/deploy-bash/redhat-cfg.sh
+++ user-bash/deploy-bash/redhat-cfg.sh
@@ -1,15 +1,36 @@
-# edit hostname
 echo "set ts=4" >> ~/.vimrc
 echo "alias vi=vim" >> ~/.bashrc
+
+nic_org_name=enp8s0
+nic_new_name=eth0
+nic_new_cfg=ifcfg-$nic_new_name
+ip_address=192.168.12.120
+net_mask=255.255.0.0
+gate_way=192.168.0.1
+dns_server=8.8.8.8
+
 cd /etc/sysconfig/network-scripts
-mv ifcfg-enp8s0 ifcfg-eth0
-sed -i "s/BOOTPROTO=.*/BOOTPROTO=static/g" ifcfg-eth0
-sed -i "s/NAME=.*/NAME=eth0/g" ifcfg-eth0
-#sed -i "s/ONBOOT=.*/ONBOOT=yes/g" ifcfg-eth0
-echo "IPADDR=172.23.12.120" >> ifcfg-eth0
-echo "NETMASK=255.255.0.0" >> ifcfg-eth0
-echo "GATEWAY=172.23.0.1" >> ifcfg-eth0
-echo "DNS1=166.111.8.28" >> ifcfg-eth0
+mv ifcfg-$nic_org_name $nic_new_name	# NIC name
+sed -i "s/BOOTPROTO=.*/BOOTPROTO=static/g" $nic_new_cfg
+sed -i "s/NAME=.*/NAME=$nic_new_name/g" $nic_new_cfg
+sed -i "s/ONBOOT=.*/ONBOOT=yes/g" $nic_new_cfg
+echo "IPADDR=$ip_address" >> $nic_new_cfg	# ip address
+echo "NETMASK=$net_mask" >> $nic_new_cfg	# net mask
+echo "GATEWAY=$gate_way" >> $nic_new_cfg	# gateway
+echo "DNS1=$dns_server" >> $nic_new_cfg		# dns
+
 sed -i "s/quiet/quiet net.ifnames=0 biosdevname=0/g" /etc/default/grub
 grub2-mkconfig -o /boot/grub2/grub.cfg
+
+systemctl stop firewalld
+systemctl disable firewalld
+setenforce 0
+
+sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+echo "/usr/lib" >> /etc/ld.so.conf
+echo "/usr/lib64" >> /etc/ld.so.conf
+ldconfig
+
 #reboot
--- user-bash/deploy-bash/scpBash.sh
+++
@@ -1,5 +0,0 @@
-for((i=1; i<4; i++))
-do
-	#sshpass -p 111111 scp -o StrictHostKeyChecking=no -r ~/deploy-bash node$i:~
-	scp -r ~/cetune-bash node$i:~
-done
